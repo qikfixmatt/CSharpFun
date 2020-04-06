372cbba90778ec708505b0effffec735963c8862
Matthew Morgan <mmorgan@wilsonelectronics.com>
2020-04-06 16:49:35 -0600

    Add project files.

---
+++ CSharpFun/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace CSharpFun
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
